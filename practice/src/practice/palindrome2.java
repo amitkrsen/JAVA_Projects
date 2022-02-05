07a5ad1031642627edbd370f6a7983620504be68
unknown <amit.kr.sen21@gmail.com>
2022-02-05 13:18:00 +0530

    added a class file for Palindrome.

---
+++ practice/src/practice/palindrome2.java
@@ -0,0 +1,28 @@
+package practice;
+
+import java.util.Scanner;
+
+public class palindrome2 {
+
+	public static void main(String[] args) {
+		String str, rev = "";
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a string: ");
+		str = sc.nextLine();
+		
+		int length = str.length();
+		
+		for (int i = length - 1; i >= 0; i--)
+			rev = rev + str.charAt(i);
+		
+		System.out.println(rev);
+		
+		if (str.equals(rev))
+			System.out.println("It is a Palindrome");
+		else
+			System.out.println("It is not a Palindrome");
+
+	}
+
+}
